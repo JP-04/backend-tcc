ba17df066942d3f22f9c248764cacd4a64c74bbd
Etec <etec@etec.com>
2022-06-10 17:41:09 -0300

    implementando css

--- cadastro.php
+++ cadastro.php
@@ -7,11 +7,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <title>Cadastro</title>
 </head>
 <body>
-    <a href="./login.php">login</a>
-    <form action="cadastro.php" method="POST">
+    <a href=".php">login</a>
+    <form action="cadastro.php" method="POST" id="form">
 
         <h1>CADASTRO PSICOKIDS</h1>
 
